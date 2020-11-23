f196857b21b30fc065a3af2de7f01d09fea75f58
Ankita Thomas <ankithom@redhat.com>
2020-11-23 07:27:53 -0500

    update push script

--- scripts/push_upstream.sh
+++ scripts/push_upstream.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 
-source ./utils.sh
+set -eu
 
-if [ $# -lt 1 ]; then
-	echo "usage: $0 <subtree to push> [<ref>]"
+source "$(dirname $0)/utils.sh"
+
+if [ $# -lt 2 ]; then
+	echo "Push a commit range or ref to a staged upstream repository"
+	echo "usage: $0 <subtree to push> <ref>"
 	exit 0
 fi
 
 repodir=$1
 remote="$repodir"
-versionfile="$repodir.UPSTREAM_VERSION"
-
-git diff-index --quiet HEAD || exit_on_error "Git status not clean, aborting !!\\n\\n$(git status)"
-
-if [ $# -gt 2 ]; then
-	ref=$2
-else
-	HEAD=$(git rev-parse HEAD)
-	ls "$repodir.UPSTREAM_VERSION" || exit_on_error "upstream version file missing, "
-	if [ ! -f "$versionfile" ]; then
-		echo "$versionfile file missing, pushing HEAD"
-		ref="$HEAD"	
-	else
-		ref="$(cat "$versionfile")..$HEAD"
-		if [ $ref == "..$HEAD" ]; then
-			echo "$versionfile file empty, pushing HEAD"
-			ref="$HEAD"
-		fi
-	fi
-fi
+ref=$2
 
 shortref=$(echo "$ref" | sed 's/.*\.\([^.]\{,6\}\)[^.]*/\1/')
 newbranch="$repodir-$shortref-$(date "+%s")"
