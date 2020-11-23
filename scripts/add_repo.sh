5157e057c1f79d3bcfdab85346b94fce3f9f480b
Ankita Thomas <ankithom@redhat.com>
2020-11-23 05:49:16 -0500

    update scripts

--- scripts/add_repo.sh
+++ scripts/add_repo.sh
@@ -4,25 +4,14 @@ set -eu
 
 source ./utils.sh
 
-trap 'exit_on_error "$0:" $?' ERR
-
-staging_dir="../staging"
-repo_list="tracked"
 
 case "$*" in
 	*" -h"*|"-h"|*" --help"*|"--help"|"")
+	echo "Add a new upstream repository to track in the $staging_dir directory"
 	echo "usage: $0 <git remote url>"
 	exit 0
 	;;
 esac
-
-if [ ! -f $repo_list ]; then
-	touch $repo_list
-fi
-
-if [ ! -d "$staging_dir" ]; then
-	mkdir $staging_dir
-fi
 
 # check if remote is valid
 git ls-remote $1 
@@ -30,6 +19,11 @@ git ls-remote $1
 remote_url=$1
 
 remote_name=$(echo $remote_url | sed 's!.*/\([^\/]*\)!\1!' | sed 's/.git$//')
+remote_dir="$staging_dir/$remote_name"
+update_tracked=false
+add_subtree=false
+
+
 if [ "|$remote_name|" == "||" ]; then
 	exit_on_error "cannot get repository name from $remote_url"
 fi
@@ -42,7 +36,7 @@ if [ $(grep "$remote_name " $repo_list | sed 's/.* \(.*\)/\1/' | wc -c) -gt 0 ];
 		remote_url=$tracked_url
 	fi
 else
-	echo "$remote_name $remote_url" >> $repo_list
+	update_tracked=true
 fi
 
 if [ $(git remote get-url $remote_name | wc -c) -eq 0 ]; then
@@ -50,20 +44,43 @@ if [ $(git remote get-url $remote_name | wc -c) -eq 0 ]; then
 fi
 
 # check if directory is present on subtree_dir
-if [ ! -d "$staging_dir/$remote_name" ]; then
+if [ ! -d "$remote_dir" ]; then
 	git rev-parse --symbolic-full-name --abbrev-ref HEAD >/dev/null 2>&1 || exit_on_error "invalid ref HEAD, cannot add subtree" $?
 
 	git diff-index --quiet HEAD || exit_on_error "Git status not clean, aborting !!\\n\\n$(git status)" $?
 
 	# repo either newly created
-	ref=$(git show-ref remotes/$dir/master -s)
-	git subtree add --prefix="$dir/" "$dir" --squash master
+	ref=$(git show-ref remotes/$remote_name/master -s)
+	git subtree add --prefix="$remote_dir" "$remote_name" --squash master
 	echo "Added new subtree $repo"
-	git commit -m "tracking new subtree $dir"
+	git commit -m "tracking new subtree $remote_dir"
+	add_subtree=true
 else
-	echo "$staging_dir/$remote_name already exists"
+	echo "$remote_dir already exists"
 fi
 
+if [ $update_tracked ]; then
+	echo "$remote_name $remote_url" >> $repo_list
+	git add $repo_list
+	if [ $add_subtree ]; then
+		git commit --amend --no-edit
+	else
+		git commit -m "update tracked remotes for $remote_name"
+	fi
+fi
+
+if [ $add_subtree ] || [ $update_tracked ]; then
+	# push to subtree dir
+	FORK_REMOTE=${FORK_REMOTE:-origin}
+	git push ${FORK_REMOTE} ${temp_branch}
+	
+	echo "Pushed changes to ${FORK_REMOTE} ${temp_branch}"
+	echo "You can now create a PR for the update"
+fi
+
+cleanup_and_reset_branch
+
+exit 0
 for repo in $(cat tracked); do
 	dir=$(echo "$repo" | sed 's!.*/\(.*\)!\1!')
 	git_repo_url="git@github.com:$repo.git"
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,22 +1,78 @@
 #!/bin/bash
 
+function cleanup_and_reset_branch {
+	if [ -n ${temp_branch} ]; then
+		git checkout -f "${current_branch}" >/dev/null 2>&1 || true
+		git branch -D "${temp_branch}" >/dev/null 2>&1 || true
+	fi
+	if [[ -n "${prtext}" ]]; then
+		rm "${prtext}"
+	fi
+}
+
 function exit_on_error {
         last="$#"
         code=1
         if [ $# -gt 0 ]; then
                 usr_code=$(eval echo "\${$last}")
-                if [[ "$usr_code" =~ ^[0-9]+$ ]]; then
+                if [[ "${usr_code}" =~ ^[0-9]+$ ]]; then
                         last=$(( $# - 1 ))
-                        code=$usr_code
+                        code=${usr_code}
                 fi
         fi
         if [ -z $code ]; then
                 return
         fi
         msg="[ERROR] line $(caller)"
-        if [ $last -gt -1 ]; then
-                msg="$msg: ${@:1:$last}"
+        if [ ${last} -gt -1 ]; then
+                msg="${msg}: ${@:1:${last}}"
         fi
-        echo -e "\e[91m$msg\e[0m"
-        exit $code
+        echo -e "\e[91m${msg}\e[0m"
+	cleanup_and_reset_branch
+        exit ${code}
 }
+
+
+if [[ -z ${GITHUB_USER:-} ]]; then
+  exit_on_error "Please export GITHUB_USER=<your-user> (or GH organization, if that's where your fork lives)"
+fi
+
+if ! which git > /dev/null; then
+  exit_on_error "Can't find git in PATH"
+fi
+
+git subtree &>/dev/null
+if [ $? -eq 1 ]; then
+	exit_on_error "installed git version does not support subtree command, please see https://github.com/git/git/tree/master/contrib/subtree"
+fi
+
+if git_status=$(git status --porcelain --untracked=no 2>/dev/null) && [[ -n "${git_status}" ]]; then
+  exit_on_error "Git status not clean, aborting !!\\n\\n$(git status)" $?
+  exit 1
+fi
+
+trap 'exit_on_error "$0:" $?' ERR
+
+current_branch=$(git symbolic-ref --short HEAD)
+repo_root=$(git rev-parse --show-toplevel)
+staging_dir="staging"
+repo_list="scripts/tracked"
+temp_branch="automated-sync-$(date +%s)"
+
+cd ${repo_root}
+
+if [[ -e ".git/rebase-apply" ]]; then
+  exit_on_error "!!! 'git rebase' or 'git am' in progress, aborting"
+fi
+
+git checkout -b ${temp_branch}
+
+if [ ! -f ${repo_list} ]; then
+	touch ${repo_list}
+fi
+
+if [ ! -d "${staging_dir}" ]; then
+	mkdir ${staging_dir}
+fi
+
+cleanup_and_reset_branch
