a3f7d1569b247c942386f50163130d595b30c8d8
Ankita Thomas <ankithom@redhat.com>
2020-11-23 15:06:29 -0500

    update pull script

--- scripts/pull_upstream.sh
+++ scripts/pull_upstream.sh
@@ -11,32 +11,12 @@ source "$(dirname $0)/utils.sh"
 while read -r line; do
         remote_name=$(echo "$line" | awk '{print $1}')
 	git fetch -t $remote_name master
+	split_branch="$remote_name-$(date +%s)"
+	git subtree split --prefix=$staging_dir/$remote_name --rejoin -b $split_branch 
 
-	ref=$(git rev-parse "FETCH_HEAD")
-
-	git merge --no-commit -Xsubtree=$repodir $ref
-
-	unmerged_files=$(git diff --name-only --diff-filter=U --exit-code)
-	differences=$?
-	# TODO: amend and continue, instead of prefering theirs
-	if [[ $differences -eq 1 ]]; then
-	  unmerged_files_oneline=$(echo "$unmerged_files" | paste -s -d ' ')
-	  git checkout --theirs -- $unmerged_files_oneline
-	  if [[ $(git diff --check) ]]; then
-	    echo "All conflict markers should have been taken care of, aborting."
-	    exit 1
-	  fi
-	  git add -- $unmerged_files_oneline
-	else
-	  unmerged_files="<NONE>"
-	fi
+	git subtree pull --squash -m "Sync upstream $remote_name" --prefix=$staging_dir/$remote_name $remote_name master
+	git branch -D $split_branch || true
 done <$repo_list
-
-# just to make sure an old version merge is not being made
-git diff --staged --quiet && { echo "No changed files in merge?! Aborting."; exit 0; }
-
-# make local commit
-git commit -m "Sync upstream master" 
 
 printf "\\n** Upstream merge complete! **\\n"
 echo "$ git checkout $temp_branch"
